51897f6bb88f7522e178ba7d6dd41ad0058a15d6
Jeff McMahan <mcmahan.jeff@gmail.com>
2017-03-07 20:15:12 -0600

    code for giphy ajax call & append to doc added

--- javascript/app.js
+++ javascript/app.js
@@ -1,9 +1,4 @@
-$(document).ready(function() {
-
-var test = 123;
-console.log(test);
-
-// firebase initialize
+// firebase initialize for table database
 var config = {
     apiKey: "AIzaSyC8qOlu41DAJ_qJEKOhOQ95XcD9JRXWLbY",
     authDomain: "gp1-hookedonafeeling.firebaseapp.com",
@@ -11,6 +6,7 @@ var config = {
     storageBucket: "gp1-hookedonafeeling.appspot.com",
     messagingSenderId: "236483189922"
 };
+
 firebase.initializeApp(config);
 
 var database = firebase.database();
@@ -19,7 +15,24 @@ var loggedDate = "";		//<---variables for diary/tabel
 var loggedMood = "";
 var loggedVidLink = "";
 var currentDateTime = moment(); //<--varibale for posting current date-time in various places on site
+console.log(moment(currentDateTime).format("MM/DD/YYYY"));
 
+
+
+//Giphy ajax call for mood
+var mood = "excited";
+var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + mood + "&api_key=dc6zaTOxFJmzC&limit=1";
+
+$.ajax({                     
+        url: queryURL,
+        method: "GET"
+    })
+    .done(function(response) { 
+        var results = response.data;
+        console.log(response.data)
+		var moodGif = $("<img>");
+		moodGif.attr("src", results[0].images.fixed_height.url);
+		$("#gifDiv").append(moodGif);
 
 
 });
