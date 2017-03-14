a05564ff26179ddf41009b68de35ee700248362b
Jeff McMahan <mcmahan.jeff@gmail.com>
2017-03-14 18:48:58 -0500

    public js

--- public/javascript/app.js
+++ public/javascript/app.js
@@ -26,7 +26,6 @@ firebase.initializeApp(config);
          var credential = error.credential;
          // ...
     });
-
 // click event for user loging, uses google accout stores user accout id in userId variable for later use
 // initalizes firebase db and calls tableBuild function to display stored user input
 $('#login').on('click', function(userId, database){
@@ -40,8 +39,7 @@ $('#login').on('click', function(userId, database){
 // mood dropdown menu conrtol
 $('.dropdown-menu li a').on('click', function(){
     $('#currentMood').val($(this).text());
-    console.log(($(this).text()));
-    $('#currentMood').html("Current Mood: " + ($(this).text()));
+    $('#currentMood').html("Today's Mood: " + ($(this).text()));
 });
 
 // on click verifies correct date format is used and mood/data/commets are added to firebase
@@ -66,6 +64,7 @@ $("#addMoodButton").on("click", function(){
     }
 });
 
+
 function tableBuild(){
     var database = firebase.database();
     var userId = firebase.auth().currentUser.uid;
@@ -83,32 +82,45 @@ function tableBuild(){
         console.log("Error: " + errorObj.code);
     });
 };
+
 // Leigh's code for css change //
-/*    $('.dropdown-menu li a').on('click', function(){
-        $('#currentMood').val($(this).text());
-
-           if (mood == 'Happy') {
-           } else if (mood =='Sad') {
-           } else if (mood == 'Mad') {
-           } else if (mood == 'Excited') {
-           } else if (mood == 'Tired'){
-           }*/
-
-database.ref().on("child_added", function(childSnapShot){
-    var tblRow = $('<tr>');
-    var urlLoggedVidLink = $("<href>");
-    urlLoggedVidLink.attr("href", childSnapShot.val().loggedVidLink);
-    console.log(urlLoggedVidLink);
-    tblRow.append('<td>' + childSnapShot.val().loggedDate + '</td>');
-    tblRow.append('<td>' + childSnapShot.val().loggedMood + '</td>');
-    tblRow.append('<td>' + "<a href=" + childSnapShot.val().loggedVidLink + ">YouTube Link</a></td>");
-    tblRow.append('<td>' + childSnapShot.val().loggedComment + '</td>');
-    
-    $("#moodTable").append(tblRow);
-
-}, function(errorObj){
-    console.log("Error: " + errorObj.code);
-});
+        //happy
+    $("#happy").click(function(){
+        $(".jumbotron").css("background-color", "#2e5bce");
+        $("#currentMood").css("background-color", "#f8f7be");
+        $("#addMoodButton").css("background-color", "#f8f7be");
+        $(".panel-heading").css("background-color", "#f8f7be");
+        //$(".body").css("background-image: url", "../images/newjoy.jpg");
+    });
+    $("#sad").click(function(){
+        $(".jumbotron").css("background-color", "#044f67");
+        $("#currentMood").css("background-color", "#37bc9b");
+        $("#addMoodButton").css("background-color", "#37bc9b");
+        $(".panel-heading").css("background-color", "#37bc9b");
+        //$(".body").css("background-image: url", "../images/newsadness.jpg");
+    });
+    $("#mad").click(function(){
+        $(".jumbotron").css("background-color", "#800a0a"); 
+        $("#currentMood").css("background-color", "#d93013");
+        $("#addMoodButton").css("background-color", "#d93013");
+        $(".panel-heading").css("background-color", "#d93013");
+        //$(".body").css("background-image: url", "../images/newanger.jpg");
+    });
+    $("#excited").click(function(){
+        $(".jumbotron").css("background-color", "#fdcd4f");
+        $("#currentMood").css("background-color", "#a22678");
+        $("#addMoodButton").css("background-color", "#a22678");
+        $(".panel-heading").css("background-color", "#a22678");
+        //$(".body").css("background-image: url", "../images/newbingbong.jpg");
+    });
+    $("#tired").click(function(){
+        $(".jumbotron").css("background-color", "#034002");
+        $("#currentMood").css("background-color", "#A0D468");
+        $("#addMoodButton").css("background-color", "#A0D468");
+        $(".panel-heading").css("background-color", "#A0D468");
+        //$(".body").css("background-image: url", "../images/newdisgust.jpg");
+    });
+//end leigh's code
 
 // function to push data to firebase
 function firebaseMood(mood, url, userId, database){
@@ -158,7 +170,7 @@ function displayVideo(mood) {
     var playlistId;
 
     if (mood == 'Happy') {
-        playlistId = 'PL8vILzn50tsyECzBFC5UFYDnnX07TA7wX'; //happy playlist on leighs youtube channel
+        playlistId = 'PL8vILzn50tszzH4CelbiUyWsTiY-3YF32'; //happy playlist on leighs youtube channel
     } else if (mood =='Sad') {
         playlistId = 'PL8vILzn50tsyKw_P4pRtT51tokZ0OFzAL'; //sad playlist on leighs youtube channel
     } else if (mood == 'Mad') {
